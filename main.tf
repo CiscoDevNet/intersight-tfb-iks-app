040add85594f7c40b45ffac1e378228a7f810fe3
prathjan <prathjan@cisco.com>
2021-04-15 15:22:44 -0700

    mainTF

---
+++ main.tf
@@ -0,0 +1,41 @@
+#Helm install of sample app on IKS
+data "terraform_remote_state" "iksws" {
+  backend = "remote"
+  config = {
+    organization = "CiscoDevNet"
+    workspaces = {
+      name = var.ikswsname 
+    }
+  }
+}
+
+variable "ikswsname" {
+  type = string
+}
+
+resource helm_release nginx_ingress {
+  name       = "nginx-ingress-controller"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host = local.kube_config.clusters[0].cluster.server
+    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
+    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
+    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
+  }
+}
+
+locals {
+  kube_config = yamldecode(data.terraform_remote_state.iksws.outputs.kube_config)
+}
+
+
